5d4ad1c45de4e38c1cdfc7ae8bbf5d804055b1b1
David Turner <novalis@novalis.org>
2007-10-11 19:49:49 +0000

    actually send notes, maybe
    git-svn-id: https://svn.socialplanning.org/svn/Twirlip/trunk@9458 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- twirlip/lib/notification.py
+++ twirlip/lib/notification.py
@@ -1,7 +1,9 @@
+from twirlip.lib.sendmail import send_mail
+
 def email(user, page):
     if not user.email:
         return
-    send_mail (user.email, "The page %s has been updated" % page.name)
+    send_mail ('notification@openplans.org', user.email, 'Update notification', "The page %s has been updated" % page.name)
 
 
 notification_methods = {'Email' : email}
---
+++ twirlip/lib/sendmail.py
@@ -0,0 +1,51 @@
+from email.Message import Message
+import smtplib
+from pylons import config
+import re
+
+class EmailMessage:
+    subjectline_re = re.compile("^Subject: [^\n]+$(.*)", re.S|re.M)
+    varsub_re = re.compile("${(\w+)}")
+        
+    def __init__(self, message_name, **kw):
+        import nz.lib.emailmessages as messages
+        raw_message = getattr(messages, message_name)
+        match = subjectline.re.match(raw_message)
+        if match:
+            self.subject = match.group(1)
+            raw_message = match.group(2)
+        else:
+            self.subject = ''
+
+        self.body = varsub_re.sub(lambda var: kw[var], raw_message)
+        
+
+def send_mail(sender, to, subject, body):
+    glob_email = config.get('global_email_override')
+    if glob_email:
+        to = glob_email
+    message = Message()
+    message["To"] = to
+    message["From"] = sender
+    message["Subject"] = subject
+    message.set_payload(body)
+    server = open_server()
+    server.sendmail(sender, to, message.as_string())
+    server.quit()
+
+def open_server():
+    smtp_server = config.get('smtp_server', 'localhost')
+    smtp_username = config.get('smtp_username')
+    server = smtplib.SMTP(smtp_server)
+    if smtp_username:
+        smtp_password_file = config.get('smtp_password_file')
+        if not smtp_password_file:
+            raise Exception(
+                "smtp_username is set, but not smtp_password_file")
+        f = open(smtp_password_file)
+        password = f.read().strip()
+        f.close()
+        server.login(smtp_username, password)
+    return server
+
+        
