e7c770d73d0653cb58b6c5c2da917bb1b45f6f41
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2008-04-15 16:32:59 +0000

    fix tabs
    git-svn-id: https://svn.socialplanning.org/svn/Twirlip/trunk@15793 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- twirlip/lib/messages.py
+++ twirlip/lib/messages.py
@@ -1,23 +1,23 @@
 messages = dict(
     update = dict(
-    subject = 'Update notification - %(title)s',
-    body = """The page %(title)s has been updated.  \nGo to %(url)s to see it.
+        subject = 'Update notification - %(title)s',
+        body = """The page %(title)s has been updated.  \nGo to %(url)s to see it.
 %(unsubscribe)s"""
-    ),
+        ),
     delete = dict(
-    subject = 'Delete notification - %(title)s',
-    body = """The page %(title)s has been deleted.
+        subject = 'Delete notification - %(title)s',
+        body = """The page %(title)s has been deleted.
 %(unsubscribe)s"""    
-    ),
+        ),
     create = dict(
-    subject = 'Page created - %(title)s',
-    body = """The page %(title)s has been deleted.
+        subject = 'Page created - %(title)s',
+        body = """The page %(title)s has been deleted.
 %(unsubscribe)s"""    
-    ),
+        ),
     unsubscribe = dict(
-    subject = 'unsubscribe message',
-    body = """To unsubscribe from these messages, visit
+        subject = 'unsubscribe message',
+        body = """To unsubscribe from these messages, visit
 http://www.openplans.org/people/%(user)s/account.
     """
+        )
     )
-)
