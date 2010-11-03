faf4b7414b66b529a8958120e105ed6b1748e674
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2010-11-03 19:26:10 +0000

    Fix database setup, which was failing on http://buildbot.socialplanning.org/ with the following bizarre error:
     Can't create table 'buildbot_op_full_p3twirlip.#sql-6bfe_3112' (errno: 150)
    
    The problem is mutually referencing tables; the solution is to defer execution of the constraints until after all tables are built.
    
    References:
     * http://www.mail-archive.com/sqlobject-discuss@lists.sourceforge.net/msg04082.html (diagnosis of the problem)
     * http://www.sqlobject.org/FAQ.html#mutually-referencing-tables
    
    
    
    git-svn-id: https://svn.socialplanning.org/svn/Twirlip/trunk@27727 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- twirlip/websetup.py
+++ twirlip/websetup.py
@@ -12,14 +12,20 @@ def setup_config(command, filename, section, vars):
     conf = appconfig('config:' + filename + '#' + section.split(':')[1])
     load_environment(conf.global_conf, conf.local_conf)
 
-    from twirlip.model import soClasses
+    from twirlip.model import soClasses, hub
     from twirlip.model.config import create_defaults
 
     if filename.endswith("test.ini"):
         for table in soClasses[::-1]:
             table.dropTable(ifExists=True)
+
+    constraints = []
     for table in soClasses:
-        table.createTable(ifNotExists=True)
+        constraints += table.createTable(ifNotExists=True, applyConstraints=False)
+
+    connection = hub.hub.getConnection()
+    for constraint in constraints:
+        connection.query(constraint)
 
     create_defaults()
 
